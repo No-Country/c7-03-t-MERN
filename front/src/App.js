a6c6ae3b6ffeb6a7bb845cb37ab4bd076d6b20b9
Rodrigo Palomo <repmlm@hotmail.com>
2022-09-27 18:44:52 -0300

    all

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App"> 
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
