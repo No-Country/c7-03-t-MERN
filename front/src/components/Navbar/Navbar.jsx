a2cbb5bd0301cfc53812616b565df1297287d256
Rodrigo Palomo <repmlm@hotmail.com>
2022-09-28 19:50:30 -0300

    Home Page

--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -12,9 +12,15 @@ const Navbar = () => {
             <img src={Logo} alt="Logo" className="logo" />
           </Link>
           <div className="menuNavegacion">
-          <Link to="/" className="nav">Home</Link>
+          <Link to="/" className="nav"><span>Home</span></Link>
           <Link to="/preguntas" className="nav">Preguntas Frecuentes</Link>
           <Link to="/contacto" className="nav">Contacto</Link>
+          </div>
+          <div className="containerBtnMain">
+            <div className="containerBtn">
+          <Link to="/login"><button className="inicioSesion">Iniciar Sesión</button></Link>
+          <Link to="/register"><button className="registrarse">Registrarse</button></Link>
+          </div>
           </div>
         </div>
       </div>
