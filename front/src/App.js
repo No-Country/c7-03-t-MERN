c3a7c00e89459d04f12ecb9e3255e57180de102c
mariokarajallo <mario_karajallo@hotmail.com>
2022-09-29 12:22:02 -0400

    add Nosotros jsx

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Nosotros from "./components/Nosotros/Nosotros";
 
 function App() {
   return (
-    <div className="App"> 
-      <Navbar />
+    <div className="App">
+      {/* <Navbar /> */}
       {/* <Home /> */}
+      <Nosotros />
     </div>
   );
 }
