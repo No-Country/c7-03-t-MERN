db1c54d6550cadd6cfe223c93c3ed427b3c5b015
Rodrigo Palomo <repmlm@hotmail.com>
2022-10-05 16:54:48 -0300

    Added 404 component

---
+++ front/src/components/Error/Error.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ErrorImg from '../../assets/icons/error.svg';
+import './Error.css';
+
+const Error = () => {
+  return (
+    // Agregar los componentes navbar y header
+    <section>
+      <div className='containerError'>
+        <img src={ErrorImg} alt='Error image' className='imgError' />
+      </div>
+      <div className='containerTitle'>
+        <h1>¡Ups! No pudimos encontrar la página :(</h1>
+      </div>
+    </section>
+    // Agregar el componente footer
+  )
+}
+
+export default Error
