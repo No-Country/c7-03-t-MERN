284855c44672a4445e4c0f972f87ffa7a0aaa1b7
Diego-Nieves26 <nieves.diego0426@gmail.com>
2022-10-06 18:30:46 -0500

    deploy test number one

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Home, Question, ErrorPage,RegisterPage } from "./pages";
+import { useSelector } from "react-redux";
+import { ModalCancha } from "./components";
+import React from "react";
+import "./App.css";
 
 function App() {
+  const modalData = useSelector((state) => state.chanchaModal);
+
   return (
-    <div className="App"> 
-      <Navbar />
-      <Home />
-    </div>
+    <BrowserRouter>
+      {modalData !== null && <ModalCancha />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/preguntas" element={<Question />} />
+        <Route path="/error" element={<ErrorPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
