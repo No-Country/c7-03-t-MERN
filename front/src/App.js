7ae7f97453477d1f8321bd1a75394d747695a9b0
Rodrigo Palomo <repmlm@hotmail.com>
2022-10-06 13:21:42 -0300

    test 1

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
-// import Navbar from './components/Navbar/Navbar';
-import Home from './components/Register/Register';
+import { Home, Question } from "./pages";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import "./App.css";
+import React from "react";
+import { useSelector } from "react-redux";
+import {ModalCancha} from "./components"
 
 function App() {
+  const modalData = useSelector(state => state.chanchaModal)
+
   return (
-    <div className="App"> 
-      {/* <Navbar /> */}
-      <Home />
-    </div>
+    <BrowserRouter>
+      {modalData !== null && <ModalCancha />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/preguntas" element={<Question />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
