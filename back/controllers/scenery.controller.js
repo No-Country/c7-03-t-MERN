cdf5eb8828e965c0a2d942c8e88fa77d4744c0c8
jacksonPeaku <jackson.avila@peaku.ai>
2022-10-19 12:48:09 -0500

    imgs full add

--- back/controllers/scenery.controller.js
+++ back/controllers/scenery.controller.js
@@ -10,9 +10,9 @@ const { AppError } = require('../utils/appError.util');
 const { ref, uploadBytes, getDownloadURL } = require('firebase/storage');
 const { storage } = require('../utils/firebase');
 
-const createScenery = catchAsync(async(req, res, next) => {
+const createScenery = catchAsync(async (req, res, next) => {
 
-    const { sceneryName, location:{ lat, long }, description, rating } = req.body;
+    const { sceneryName, location: { lat, long }, description, rating } = req.body;
 
     const imgScenerysRef = ref(storage, `scenerys/${req.file.originalname}`);
     const imgScenerys = await uploadBytes(imgScenerysRef, req.file.buffer);
@@ -23,15 +23,15 @@ const createScenery = catchAsync(async(req, res, next) => {
 
     const sceneryExist = await Scenery.findOne({ sceneryName });
 
-    if( sceneryExist ){
-        return next(new AppError('busy scenery name',400));
+    if (sceneryExist) {
+        return next(new AppError('busy scenery name', 400));
     }
 
     user.password = undefined;
 
     const newScenery = await Scenery.create({
         sceneryName,
-        location:{ lat, long },
+        location: { lat, long },
         description,
         sceneryImgUrl: imgScenerys.metadata.fullPath,
         rating,
@@ -39,10 +39,43 @@ const createScenery = catchAsync(async(req, res, next) => {
     });
 
     res.status(201).json({
-        status:'success',
+        status: 'success',
         newScenery
     });
 
 });
 
-module.exports = { createScenery };
+
+const getSceneryAll = catchAsync(async (req, res, next) => {
+
+    const sceneryAlls = await Scenery.find({});
+    
+    const sceneryPromises = sceneryAlls.map(async sceneryAll => {
+        const imgRef = ref(storage, sceneryAll.sceneryImgUrl);
+        sceneryAll.sceneryImgUrl = await getDownloadURL(imgRef);
+        return  sceneryAll;
+    });
+
+    const sceneryResolved = await Promise.all( sceneryPromises);
+
+    res.status(201).json({
+        status: 'success',
+        sceneryResolved
+    })
+});
+
+const getSceneryById = catchAsync(async (req, res, next) => {
+
+    const { scenery } = req;
+
+    const imgRef = ref(storage, scenery.sceneryImgUrl);
+    scenery.sceneryImgUrl = await getDownloadURL(imgRef);
+
+    //console.log(sceneryAll[1].sceneryImgUrl)
+    res.status(201).json({
+        status: 'success',
+        scenery,
+    })
+});
+
+module.exports = { createScenery, getSceneryAll, getSceneryById };
