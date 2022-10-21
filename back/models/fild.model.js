d2eb08961ceed9f57a4753d451de4ab0a7aaad41
jacksonPeaku <jackson.avila@peaku.ai>
2022-10-21 04:50:25 -0500

    add search filter

--- back/models/fild.model.js
+++ back/models/fild.model.js
@@ -2,37 +2,41 @@ const mongoose = require('mongoose');
 
 const fildScheme = new mongoose.Schema({
 
-    nameFild: {
-        type:String,
-        required: [true, 'Please enter you name sport '],
-    },  
-    sceneryId: {
-        type:Object,
-        required: [true, 'Please enter you scenary'],
-    }, 
-    sportId:{
-        type:String,
-        require: [true, 'Please enter the sportId'],
-    },
-      accountPerson:{
-        type: Number,
-        require: [true, 'Please enter person'],
-      },
-      description:{
-        type: String,
-        require:[true, 'Please enter description'],
-      },
-    rating:{
-        type:Number,
-        required: [true, 'Please enter your rating scenery'],
-    },
-    status: {
-        type: String,
-        default: 'active',
-    },
+  nameFild: {
+    type: String,
+    required: [true, 'Please enter you name sport '],
+  },
+  sceneryId: {
+    type: String,
+    required: [true, 'Please enter you scenary'],
+  },
+  sportId: {
+    type: Object,
+    require: [true, 'Please enter the sportId'],
+  },
+  accountPerson: {
+    type: Number,
+    require: [true, 'Please enter person'],
+  },
+  description: {
+    type: String,
+    require: [true, 'Please enter description'],
+  },
+  fildImgUrl: [{
+    type: mongoose.Types.ObjectId,
+    ref: 'FildImg'
+  }],
+  rating: {
+    type: Number,
+    required: [true, 'Please enter your rating scenery'],
+  },
+  status: {
+    type: String,
+    default: 'active',
+  },
 
 });
 
-const Fild = mongoose.model( 'Fild', fildScheme );
+const Fild = mongoose.model('Fild', fildScheme);
 
 module.exports = { Fild }
